dd96e763d2fbffbd6c8466282af236d011d41be4
Marco Santana <marco.santana@gmail.com>
2019-03-28 20:15:31 -0600

    Added menu and drawer fixed minor lint

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,66 @@
+<template>
+    <span>
+        <v-toolbar app color="green darken-4" dark>
+            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer">
+                <v-icon>home</v-icon>
+            </v-toolbar-side-icon>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <v-btn flat class="hidden-sm-and-down" v-if="loged">Panel de control</v-btn>
+            <v-spacer class="hidden-sm-and-down"></v-spacer>
+            <span v-if="!loged">
+                <v-btn flat class="hidden-sm-and-down">Ingresar</v-btn>
+                <v-btn color="green lighten-3" class="hidden-sm-and-down">Registrarse</v-btn>
+            </span>
+        </v-toolbar>
+        <v-navigation-drawer app v-model="drawer" class="green lighten-2" dark disable-resize-watcher>
+            <v-list>
+                    <v-toolbar flat>
+                        <v-list>
+                            <v-list-tile>
+                                <v-list-tile-title class="title">
+                                    {{appTitle}}
+                                </v-list-tile-title>
+                            </v-list-tile>
+                        </v-list>
+                    </v-toolbar>
+                    <v-divider></v-divider>
+                    <v-list dense class="pt-0">
+                        <v-list-tile v-for="(item, index) in items" :key="item.key" @click="">
+                            <v-list-tile-action>
+                                <v-icon>{{ item.icon }}</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list>
+            </v-list>
+        </v-navigation-drawer>
+        
+    </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Elsevier Tester',
+            drawer: false,
+            loged: false,
+            items: [
+                { title: 'Panel de control' },
+                { title: 'Ingresar'},
+                { title: 'Registrarse' },
+            ],
+        };
+    },
+});
+</script>
+
+<style scoped>
+
+</style>
+
